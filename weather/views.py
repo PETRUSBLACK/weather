9ee95bc132e1b562cf77cc7abefac726b91e01e3
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-02-01 12:01:33 +0100

    adding weatherapi

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+import json
+import urllib.request 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method == "POST":
+        city = request.POST['city']
+        res = urllib.request.urlopen('https://api.api-ninjas.com/v1/weather?city=city').read()
+        json_data = json.loads(res)
+    else:
+        city = ''
+    return render(request, 'index.html', {'city' : city}) 
